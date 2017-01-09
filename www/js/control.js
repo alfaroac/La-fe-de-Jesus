4c49d29924b72f0f22a970605a13dbbb3dbca425
gitdealdo <alfaroac.is@gmail.com>
2017-01-09 16:21:57 -0500

    mejora del tema y toast

--- www/js/control.js
+++ www/js/control.js
@@ -1,4 +1,4 @@
-var lecc;
+var lecc, theme;
 function listar(arg) {
   // console.log(arg);
   var lafe = JSON.parse(localStorage.getItem("lafe"));
@@ -26,12 +26,12 @@ function listar(arg) {
   html += '<p align="center"><a class="ui-btn ui-corner-all ui-icon-mail ui-btn-icon-right ui-btn-icon-notext" onclick="mostrar_compromiso();"></a></p>';
   html += '<div id="compromiso">';
   for (var i = 0; i < lecc.compromisos.length; i++) {
-    html += '<p align="right"><a href="#vers'+i+'" data-transition="flip" data-rel="popup">'+lecc.compromisos[i].versiculo+'</a></p>';
-    html += '<textarea class="input-r" rows="3" style="display:none" id="textarea-c-'+i+'" placeholder="Ingrese versículo">'+lecc.compromisos[i].contenido+'</textarea>';
-    html += '<div data-role="popup" id="vers'+i+'" class="ui-content" style="max-width:280px">';
+    html += '<p align="right"><a href="#versiculo-'+i+'" data-transition="flip" data-rel="popup">'+lecc.compromisos[i].versiculo+'</a></p>';
+    html += '<div data-role="popup" id="versiculo-'+i+'" class="ui-content" style="max-width:280px">';
     html += '<a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a>';
     html += '<p>'+lecc.compromisos[i].contenido+'</p>';
     html += '</div>';
+    html += '<textarea class="input-r" rows="3" style="display:none" id="textarea-c-'+i+'" placeholder="Ingrese versículo">'+lecc.compromisos[i].contenido+'</textarea>';
     html += '<label for="checkbox-'+i+'">'+lecc.compromisos[i].pregunta+'</label>';
     if (lecc.compromisos[i].acepta) {
       html += '<input name="checkbox-'+i+'" id="checkbox-'+i+'" type="checkbox" checked>';
@@ -41,7 +41,14 @@ function listar(arg) {
   }
   html += '</div>';
   $(".principal").html(html);
+  cambiar_tema(theme);
   $("#compromiso").hide();
+  refresh_page();
+}
+
+function refresh_page() {
+  $("#leccion").page('destroy').page();
+  console.log("Pagina refrescada");
 }
 
 function mostrar_compromiso() {
@@ -67,24 +74,66 @@ function guardar() {
   }
   for (var i = 0; i < lecc.compromisos.length; i++) {
     // if ($('#checkbox-'+i+'').is(':checked')) {};
-    lecc.compromisos[i].acepta = $('#checkbox-'+i).is(':checked');
     lecc.compromisos[i].contenido = $('#textarea-c-'+i).val();
+    lecc.compromisos[i].acepta = $('#checkbox-'+i).is(':checked');
   }
   var lafe = JSON.parse(localStorage.getItem("lafe"));
   for (var i = 0; i < lafe.length; i++) {
-    if (lafe[i].id==lecc.id) {
+    if (lafe[i].id===lecc.id) {
       lafe[i]=lecc;
     }
   }
   localStorage.setItem('lafe', JSON.stringify(lafe));
   listar(lecc.id);
+  toast("Registro actualizado con éxito!");
 }
 
 function limpiar() {
-    localStorage.clear();
+  localStorage.clear();
+  toast("Datos vaciados!");  
 }
 
 function cargar_datos(){
   localStorage.setItem('lafe', JSON.stringify(lafe));
-  console.log("Datos cargados");  
+  toast("Datos cargados con éxito!");  
+}
+
+function toast(message) {
+  var $toast = $('<div class="ui-loader ui-overlay-shadow ui-body-e ui-corner-all"><h3>' + message + '</h3></div>');
+
+  $toast.css({
+      display: 'block', 
+      background: '#fff',
+      opacity: 0.90, 
+      position: 'fixed',
+      padding: '7px',
+      'text-align': 'center',
+      width: '270px',
+      left: ($(window).width() - 284) / 2,
+      top: $(window).height() / 2 - 20
+  });
+
+  var removeToast = function(){
+      $(this).remove();
+  };
+
+  $toast.click(removeToast);
+
+  $toast.appendTo($.mobile.pageContainer).delay(2000);
+  $toast.fadeOut(400, removeToast);
+}
+
+function cambiar_tema(arg) {
+  theme = arg;
+  $.mobile.activePage.find('.ui-btn')
+                     .removeClass('ui-btn-up-a ui-btn-up-b ui-btn-up-c ui-btn-up-d ui-btn-up-e ui-btn-hover-a ui-btn-hover-b ui-btn-hover-c ui-btn-hover-d ui-btn-hover-e')
+                     .addClass('ui-btn-up-' + theme)
+                     .attr('data-theme', theme);
+  $.mobile.activePage.find('.ui-header, .ui-footer')
+                     .removeClass('ui-bar-a ui-bar-b ui-bar-c ui-bar-d ui-bar-e')
+                     .addClass('ui-bar-' + theme)
+                     .attr('data-theme', theme);
+  $.mobile.activePage.removeClass('ui-body-a ui-body-b ui-body-c ui-body-d ui-body-e')
+                     .addClass('ui-body-' + theme)
+                     .attr('data-theme', theme);
 }
